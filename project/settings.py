4a0b78bcb5284dc016830889e3d5ea198116df57
eternel <eternel7@gmail.com>
2018-08-19 21:09:18 +0200

    test for heroku deployment

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kosh.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
