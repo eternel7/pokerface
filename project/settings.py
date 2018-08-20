d1d3ee0f233f2b921a261d0c2c6bf9ca19e3cb08
eternel <eternel7@gmail.com>
2018-08-20 11:46:47 +0200

    test for heroku deployment

--- project/settings.py
+++ project/settings.py
@@ -200,6 +200,7 @@ STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'public')
+
 WEBPACK_LOADER = {
   'DEFAULT': {
     'BUNDLE_DIR_NAME': 'dist/',
