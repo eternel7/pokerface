27e892cddb341194cd79cd6696dec109a5ace206
eternel <eternel7@gmail.com>
2018-08-26 19:53:11 +0200

    revert

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,6 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-SECURE_SSL_REDIRECT = True
 
 ALLOWED_HOSTS = ['kosh.herokuapp.com', 'localhost', '127.0.0.1']
 
